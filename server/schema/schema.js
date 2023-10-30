91731acb6b39e47cf9c7f176508e1b5fe36b353a
i2pieo <jawadsyed406@gmail.com>
2023-10-30 19:41:39 +0300

    Client Type and Queries

---
+++ server/schema/schema.js
@@ -0,0 +1,31 @@
+const { projects, clients } = require("../sampleData.js");
+
+const { GraphQLObjectType, GraphQLID, GraphQLString } = require("graphql");
+
+// Client Type
+const ClientType = new GraphQLObjectType({
+  name: "Client",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    clients: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return clients.find((client) => client.id === args.id);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
