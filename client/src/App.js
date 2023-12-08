a7539277b1f568a78a0801c0077aa7ae07861bde
Syed Jawad <jawadsyed406@gmail.com>
2023-12-08 19:02:41 +0300

    Setting Up Apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import Header from "./components/Header";
+import { ApolloProvider, ApolloClient, InMemoryCache }
+from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
+      <ApolloProvider client={client}>
       <Header />
     <div className="container">
       <h1>Hello World</h1>
     </div>
+    </ApolloProvider>
     </>
   );
 }
